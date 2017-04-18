14d8bd758bc0dd39a8aac877c0823d639a416398
Smiril <Smiril@users.noreply.github.com>
2017-04-18 20:43:06 +0200

    Update ShellShockBrowser.c

--- tools/ShellShockBrowser.c
+++ tools/ShellShockBrowser.c
@@ -21,60 +21,13 @@
 #include <openssl/err.h>
 
 void *connection_handler(void *socket_desc);
+int inet_pton(int af, const char *src, void *dst);
+const char *inet_ntop(int af, const void *src, char *dst, socklen_t size);
 int create_tcp_socket();
 char *get_ip(char *host);
 char *build_get_query(char *host, char *page);
 void iam();
 void usage();
-
-#ifdef __WIN32__
-int inet_pton(int af, const char *src, void *dst)
-{
-  struct sockaddr_storage ss;
-  int size = sizeof(ss);
-  char src_copy[INET6_ADDRSTRLEN+1];
-
-  ZeroMemory(&ss, sizeof(ss));
-  /* stupid non-const API */
-  strncpy (src_copy, src, INET6_ADDRSTRLEN+1);
-  src_copy[INET6_ADDRSTRLEN] = 0;
-
-  if (WSAStringToAddress(src_copy, af, NULL, (struct sockaddr *)&ss, &size) == 0) {
-    switch(af) {
-      case AF_INET:
-    *(struct in_addr *)dst = ((struct sockaddr_in *)&ss)->sin_addr;
-    return 1;
-      case AF_INET6:
-    *(struct in6_addr *)dst = ((struct sockaddr_in6 *)&ss)->sin6_addr;
-    return 1;
-    }
-  }
-  return 0;
-}
-
-const char *inet_ntop(int af, const void *src, char *dst, socklen_t size)
-{
-  struct sockaddr_storage ss;
-  unsigned long s = size;
-
-  ZeroMemory(&ss, sizeof(ss));
-  ss.ss_family = af;
-
-  switch(af) {
-    case AF_INET:
-      ((struct sockaddr_in *)&ss)->sin_addr = *(struct in_addr *)src;
-      break;
-    case AF_INET6:
-      ((struct sockaddr_in6 *)&ss)->sin6_addr = *(struct in6_addr *)src;
-      break;
-    default:
-      return NULL;
-  }
-  /* cannot direclty use &size because of strict aliasing rules */
-  return (WSAAddressToString((struct sockaddr *)&ss, sizeof(ss), NULL, dst, &s) == 0)?
-          dst : NULL;
-}
-#endif
 
 #define HOST "coding.debuntu.org"
 #define PAGE "index.cgi"
@@ -356,6 +309,55 @@ void *connection_handler(void *socket_desc){
   return 0;
 }
 
+#ifdef __WIN32__
+int inet_pton(int af, const char *src, void *dst)
+{
+  struct sockaddr_storage ss;
+  int size = sizeof(ss);
+  char src_copy[INET6_ADDRSTRLEN+1];
+
+  ZeroMemory(&ss, sizeof(ss));
+  /* stupid non-const API */
+  strncpy (src_copy, src, INET6_ADDRSTRLEN+1);
+  src_copy[INET6_ADDRSTRLEN] = 0;
+
+  if (WSAStringToAddress(src_copy, af, NULL, (struct sockaddr *)&ss, &size) == 0) {
+    switch(af) {
+      case AF_INET:
+    *(struct in_addr *)dst = ((struct sockaddr_in *)&ss)->sin_addr;
+    return 1;
+      case AF_INET6:
+    *(struct in6_addr *)dst = ((struct sockaddr_in6 *)&ss)->sin6_addr;
+    return 1;
+    }
+  }
+  return 0;
+}
+
+const char *inet_ntop(int af, const void *src, char *dst, socklen_t size)
+{
+  struct sockaddr_storage ss;
+  unsigned long s = size;
+
+  ZeroMemory(&ss, sizeof(ss));
+  ss.ss_family = af;
+
+  switch(af) {
+    case AF_INET:
+      ((struct sockaddr_in *)&ss)->sin_addr = *(struct in_addr *)src;
+      break;
+    case AF_INET6:
+      ((struct sockaddr_in6 *)&ss)->sin6_addr = *(struct in6_addr *)src;
+      break;
+    default:
+      return NULL;
+  }
+  /* cannot direclty use &size because of strict aliasing rules */
+  return (WSAAddressToString((struct sockaddr *)&ss, sizeof(ss), NULL, dst, &s) == 0)?
+          dst : NULL;
+}
+#endif
+
 void iam()
 {
   	  printf("\t\t.------..------..------..------..------..------.\n");
@@ -372,7 +374,7 @@ void usage()
   fprintf(stderr, "\n\x1b[33m\tUSAGE: %s HOST PAGE PORT SERVICE TARGET\n\n\
 \t\t\x1b[31m exploiting shellshock CVE-2014-6271... \n\n\
 \tHOST: the website hostname. ex: smiril.github.io/peer-chat/tools/ \n\
-\tPAGE: the page to retrieve. ex: cgi-bin/index.cgi, default: /\n\
+\tPAGE: the page to retrieve. ex: cgi-bin/bin, default: /\n\
 \tPORT: 80 , 443 \n\
 \tSERVICE: http , https \n\
 \tTARGET: the target hostname \n\n\x1b[0m",appli);
