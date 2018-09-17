e80c99a365bb84d531e74fe97e75f313d9bedab2
Smiril <Smiril@users.noreply.github.com>
2018-09-17 19:37:00 +0200

    Update Enigma2.sh

--- tools/Enigma2.sh
+++ tools/Enigma2.sh
@@ -35,7 +35,7 @@ echo " +************************************************************************
 
 echo " +****************************************************************************+
    Written by  Smiril
-   8 lines Info , 20 lines License , 1045 lines Code 
+   8 lines Info , 20 lines License , 1048 lines Code 
    @ https://github.com/Smiril/peer-chat/tree/master/tools/Enigma2.sh"
 
 echo " +****************************************************************************+
