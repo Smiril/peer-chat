71d561d108f326b9b95e54161df483fa10647cfc
Smiril <Smiril@users.noreply.github.com>
2021-02-10 21:58:03 +0100

    Create wifi.sh

---
+++ tools/wifi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# ----------------------
+# do not edit below here 
+# ----------------------
+# Startverzögerung
+echo "Scanner for Cli"
+echo ""
+i=5
+while [[ $i -gt 0 ]]; do
+	sleep 1
+	echo "starts in "$i
+	i=$[$i-1]
+done
+echo ""
+while true; do
+    echo "Scanning ..."
+    sudo iwlist wlo1 scanning | grep "Address" | awk '{print $5}' && sudo iwlist wlo1 scanning | grep "ESSID:" | awk '{print $1}'
+    NUMOFLINES=$(sudo iwlist wlo1 scanning | grep "Address" | awk '{print $5}' && sudo iwlist wlo1 scanning | grep "ESSID:" | awk '{print $1}')
+		for j in $NUMOFLINES ; do
+    sleep 1
+	done
+	done
+exit 0
